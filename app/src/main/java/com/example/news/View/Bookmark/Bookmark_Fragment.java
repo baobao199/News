0f29e54831b62ae4b21fb2ec10e8c7a839a521ab
baobao199 <nguyenbao605@gmail.com>
2020-11-25 08:13:37 +0700

    Bookmark

---
+++ app/src/main/java/com/example/news/View/Bookmark/Bookmark_Fragment.java
@@ -0,0 +1,20 @@
+package com.example.news.View.Bookmark;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.news.R;
+
+public class Bookmark_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bookmark_fragment,container,false);
+    }
+}
